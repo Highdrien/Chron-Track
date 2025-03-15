9cc4ffc9f810898ff6279fda4e47de0f7a722fe4
Highdrien <adrien.zabbanoutlook.fr>
2025-03-15 19:51:32 +0100

    add doc

--- src/iaaf.py
+++ src/iaaf.py
@@ -6,6 +6,17 @@ from type import Event, Gender, IaafModel, Time
 
 class IAAFCalculator:
     def __init__(self, data_path: Path = Path("data")) -> None:
+        """
+        Initializes the IAAF scoring formulas.
+
+        Args:
+            data_path (Path, optional): The path to the directory containing the IAAF
+                scoring formulas JSON file. Defaults to "data".
+
+        Raises:
+            FileNotFoundError: If the IAAF scoring formulas JSON file does not exist at
+                the specified path.
+        """
         self.filepath = data_path / "iaaf_scoring_formulas.json"
         if not self.filepath.exists():
             raise FileNotFoundError(
@@ -14,5 +25,16 @@ class IAAFCalculator:
         self.model = IaafModel.model_validate(json.loads(open(self.filepath).read()))
 
     def get_iaaf_score(self, gender: Gender, event: Event, time: Time) -> int:
+        """
+        Calculate the IAAF score for a given gender, event, and time.
+
+        Args:
+            gender (Gender): The gender of the athlete.
+            event (Event): The event for which the score is being calculated.
+            time (Time): The performance time of the athlete.
+
+        Returns:
+            int: The calculated IAAF score.
+        """
         coeffs = self.model.get_coeffs(gender, event)
         return coeffs.get_iaaf_score(time)
