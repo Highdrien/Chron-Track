ccfe76dede57f48fba65c7ea941c5be5b0c1e8f5
Highdrien <adrien.zabbanoutlook.fr>
2025-03-22 11:51:38 +0100

    add filter

--- app.py
+++ app.py
@@ -15,16 +15,27 @@ def load_data() -> PerfOfAllTime:
 
 st.title("Performances of All Time")
 st.write("This app allows you to track your performances over time.")
-
 perfs = load_data()
 df = perfs.table()
+
+st.sidebar.header("Filters")
+
+# Filter by location
 locations = df["location"].unique()
-
-# Ajouter une option "Tous" pour afficher toutes les courses
-selected_location = st.selectbox("Filtrer par ville :", ["Tous"] + list(locations))
-
-# Filtrer le DataFrame si une ville est sélectionnée (sauf "Tous")
-if selected_location != "Tous":
+locations.sort()
+selected_location = st.sidebar.selectbox(
+    "Filter by location :", ["All"] + list(locations)
+)
+if selected_location != "All":
     df = df[df["location"] == selected_location]
+
+# Filter by distance
+distances = df["distance"].unique()
+distances.sort()
+selected_distance = st.sidebar.selectbox(
+    "Filter by distance (km) :", ["All"] + list(distances)
+)
+if selected_distance != "All":
+    df = df[df["distance"] == selected_distance]
 
 st.dataframe(df, hide_index=True, use_container_width=True)
