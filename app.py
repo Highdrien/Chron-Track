74da8ec2c6887f91469483977e138910da0fd0aa
Highdrien <adrien.zabbanoutlook.fr>
2025-03-22 11:42:15 +0100

    add filter location

--- app.py
+++ app.py
@@ -4,13 +4,27 @@ from pathlib import Path
 from src.perfs_tracker import PerfOfAllTime
 
 
-st.title("Performance of All Time")
+def load_data() -> PerfOfAllTime:
+    filepath = Path("data/perfs.json")
+    perfs = PerfOfAllTime()
+    if filepath.exists():
+        perfs.load_from_json(Path("data/perfs.json"))
+
+    return perfs
+
+
+st.title("Performances of All Time")
 st.write("This app allows you to track your performances over time.")
 
-perfs = PerfOfAllTime()
-perfs.load_from_json(Path("data/perfs.json"))
-
+perfs = load_data()
 df = perfs.table()
-print(df)
+locations = df["location"].unique()
 
-st.dataframe(df, use_container_width=True)
+# Ajouter une option "Tous" pour afficher toutes les courses
+selected_location = st.selectbox("Filtrer par ville :", ["Tous"] + list(locations))
+
+# Filtrer le DataFrame si une ville est sélectionnée (sauf "Tous")
+if selected_location != "Tous":
+    df = df[df["location"] == selected_location]
+
+st.dataframe(df, hide_index=True, use_container_width=True)
