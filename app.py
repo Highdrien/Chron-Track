16f1754950c5f00879367d3c892018bd6780d44c
Highdrien <adrien.zabbanoutlook.fr>
2025-03-22 12:02:34 +0100

    put filter in funciton

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pathlib import Path
+import pandas as pd
 
 from src.perfs_tracker import PerfOfAllTime
 
@@ -13,29 +14,38 @@ def load_data() -> PerfOfAllTime:
     return perfs
 
 
+def filter_location(df: pd.DataFrame) -> pd.DataFrame:
+    locations = df["Location"].unique()
+    locations.sort()
+    selected_location = st.sidebar.selectbox(
+        "Filter by location :", ["All"] + list(locations)
+    )
+    if selected_location != "All":
+        df = df[df["Location"] == selected_location]
+
+    return df
+
+
+def filter_distance(df: pd.DataFrame) -> pd.DataFrame:
+    distances = df["Distance (km)"].unique()
+    distances.sort()
+    selected_distance = st.sidebar.selectbox(
+        "Filter by distance (km) :", ["All"] + list(distances)
+    )
+    if selected_distance != "All":
+        df = df[df["Distance (km)"] == selected_distance]
+
+    return df
+
+
 st.title("Performances of All Time")
 st.write("This app allows you to track your performances over time.")
 perfs = load_data()
 df = perfs.table()
 
 st.sidebar.header("Filters")
+df = filter_location(df)
+df = filter_distance(df)
 
-# Filter by location
-locations = df["Location"].unique()
-locations.sort()
-selected_location = st.sidebar.selectbox(
-    "Filter by location :", ["All"] + list(locations)
-)
-if selected_location != "All":
-    df = df[df["Location"] == selected_location]
-
-# Filter by distance
-distances = df["Distance (km)"].unique()
-distances.sort()
-selected_distance = st.sidebar.selectbox(
-    "Filter by distance (km) :", ["All"] + list(distances)
-)
-if selected_distance != "All":
-    df = df[df["Distance (km)"] == selected_distance]
 
 st.dataframe(df, hide_index=True, use_container_width=True)
